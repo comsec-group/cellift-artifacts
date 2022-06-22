0e7e7278e691745be9915ec3e128008534a3505f
Ben Gras <beng@shrike-systems.om>
2022-06-22 12:54:22 +0200

    ziprun

--- ziprun.sh
+++ ziprun.sh
@@ -6,7 +6,8 @@ set -e
 #		tar cjf ${k}.tar.bz2 $k
 	done
 )
-(cd ../cellift-meta && git diff) >build-patches/cellift-meta-patch
+#(cd ../cellift-meta && git diff) >build-patches/cellift-meta-patch
 # docker build -t ethcomsec/cellift-artifact-evaluation:cellift-artifact-evaluation-small -f Dockerfile-small . 2>&1 | tee docker-build-log1
-docker build -t ethcomsec/cellift-artifact-evaluation:cellift-artifact-evaluation-2 . 2>&1 | tee docker-build-log
+docker build -t ethcomsec/cellift-artifact-evaluation:cellift-artifact-evaluation . 2>&1 | tee docker-build-log
+docker build -t ethcomsec/cellift-artifact-evaluation:cellift-artifact-evaluation-backup . 2>&1 | tee docker-build-log-backup
 #docker build -t ethcomsec/cellift-artifact-evaluation:cellift-artifact-evaluation-2-squashed -f Dockerfile-squash . 2>&1 | tee docker-build-log-suqash
