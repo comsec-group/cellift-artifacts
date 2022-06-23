4ae15e6d803ddc453a0d3c572cfd60ce64c9def9
Ben Gras <beng@shrike-systems.com>
2022-06-23 12:58:26 +0200

    new

--- cellift-fpga-glance/run-all.sh
+++ cellift-fpga-glance/run-all.sh
@@ -6,10 +6,16 @@ do
             tcl=commands_${instr}.tcl
             (
                 cd $design
+                if [ -f ${instr}_proj/report_timing_impl.txt -a -f ${instr}_proj/report_timing.txt -a -f ${instr}_proj/report_utilization_impl.txt -a -f ${instr}_proj/report_utilization.txt ]
+                then
+                        echo " *** $design $instr report complete, skipping"
+                        continue
+                fi
+                echo " *** $design $instr not complete, synthesizing"
                 rm -rf ${instr}_proj
                 mkdir -p ${instr}_proj
                 cd ${instr}_proj
-                vivado -mode tcl -source ../$tcl
+                vivado -mode tcl -source ../$tcl 2>&1 | tee vivado-log-$design-$instr.txt
             )
         done
 done
