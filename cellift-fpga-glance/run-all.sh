def83bfb4abb923da1906cf391d570528a02f4a4
Ben Gras <beng@shrike-systems.com>
2022-07-05 15:47:10 +0200

    run-all improvements

--- cellift-fpga-glance/run-all.sh
+++ cellift-fpga-glance/run-all.sh
@@ -9,20 +9,22 @@ do
                 tfile=${instr}_proj/report_timing_impl.txt
                 ufile=${instr}_proj/report_utilization_impl.txt
                 ok=yes
-                if [ ! -f $tfile -o ! -f $ufile ]
+                if [ ! -s $tfile -o ! -s $ufile ]
                 then
                         echo " *** $design $instr reports missing"
                         ok=no
                 fi
-                if ! grep 'report_timing: Time' $PWD/$tfile; then ok=no; fi
-                if ! grep 'report_utilization: Time' $PWD/$ufile; then ok=no; fi
+                # Did synthesis exit cleanly?
+                if ! grep -q '# report_utilization' ${instr}_proj/vivado.log 2>/dev/null; then ok=no; fi
+                if ! grep -q '# report_timing' ${instr}_proj/vivado.log 2>/dev/null; then ok=no; fi
+                if ! grep -q '# exit' ${instr}_proj/vivado.log 2>/dev/null; then ok=no; fi
                 if [ $ok = yes ]
                 then
-                        echo " *** $design $instr reports complete"
+                        echo " *** $design $instr reports complete, skipping synthesis"
                         continue
                 fi
-                bzip2 -k -d -f *.bz2
                 echo " *** $design $instr not complete, synthesizing"
+                bzip2 -k -d -f *.bz2
                 rm -rf ${instr}_proj
                 mkdir -p ${instr}_proj
                 cd ${instr}_proj
